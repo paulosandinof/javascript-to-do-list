95db4f6f75493aa834413a4bb91e4ecf5e8a7b91
Paulo Sandino <paulosandino@hotmail.com>
2020-10-20 21:33:07 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen('3000');
